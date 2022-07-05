b5ea26b64ba780e18d1148342e344e4b5074f177
Timgo-1 <timgodesigns@gmail.com>
2022-07-06 02:04:26 +0300

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <foot className="footer">
+        <h4>©2022 All Rights Reserved.</h4>
+    </foot>
+  );
+}
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Our mission is to illuminate and transform how we 
+        think about cocktails by providing an array of options 
+        together with its attendant details, ranging 
+        from full proof custom bar quality drinks for 
+        your next intimate gathering, to refreshing, 
+        low ABV canned classics ready for your next outing.
+      </p>
+    </section>
+  );
+}
